486c70368e471cc4d3063cf3544cfda056d1157c
mr_celviano <nikita.sergienko@enkod.io>
2022-02-13 16:56:57 +0300

    add_sql

--- internal/sql/create_task_table.sql
+++ internal/sql/create_task_table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE task (
     id serial4 NOT NULL,
     run_time int4 NOT NULL DEFAULT 0,
-    email text NOT NULL DEFAULT ''::text,
-    "name" text NOT NULL DEFAULT ''::text,
-    CONSTRAINT task_pkey PRIMARY KEY (id)
+    user_id int4 NULL,
+    CONSTRAINT task_pkey PRIMARY KEY (id),
+    CONSTRAINT task_to_users_fkey FOREIGN KEY (user_id) REFERENCES public.users(id) ON DELETE CASCADE
 );
